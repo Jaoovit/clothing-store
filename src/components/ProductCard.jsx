29e99376307520f5020402dc56d07426a7db0e4e
João Oliveira <jaoovit@gmail.com>
2024-07-12 17:32:14 +0100

    fetch the app items and display in the homepage

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,17 @@
-const ProductCard = () => {
+import { Link } from "react-router-dom"
+import { FaEye } from "react-icons/fa";
+
+const ProductCard = ({clothes, showDetails = false}) => {
+
+  const linkToClothing = `product/${clothes.id}`
+
   return (
     <div>
-      
+        <img src={clothes.image} alt="" />
+        <p>{clothes.title}</p>
+        <p>€ {clothes.price}</p>
+        <Link to={linkToClothing}><FaEye /></Link>
+        <button>+</button>
     </div>
   )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react"
+import ProductCard from "../components/ProductCard";
+
+const clothesURL = "https://fakestoreapi.com/products"
+
 const Homepage = () => {
+
+  const [clothes, setClothes] = useState([]);
+
+  const getClothes = async (url) => {
+    const resp = await fetch(url)
+    const data = await resp.json()
+    setClothes(data)
+  }
+
+  useEffect(() => {
+    getClothes(clothesURL)
+  }, [])
+
   return (
     <div>
-      
+      {clothes.map((clothes) => <ProductCard key={clothes.id} clothes={clothes}/>)}
     </div>
   )
 }
