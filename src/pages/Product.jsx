852a2c529c850c7a16b69c65cde4f180a05b3fa3
João Oliveira <jaoovit@gmail.com>
2024-07-13 18:50:07 +0100

    create the root to look each item inside the store

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+
+const onlyOneClothingURL = "https://fakestoreapi.com/products/"
+
 const Product = () => {
+
+  const { id } = useParams();
+  const [onlyOneClothing, setOnlyOneClothing] = useState(null);
+
+  const getOnlyOneClothing = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setOnlyOneClothing(data)
+  }
+
+  useEffect(() => {
+    getOnlyOneClothing(onlyOneClothingURL + id)
+  }, [])
+
   return (
     <div>
-      
+      {onlyOneClothing && <p key={onlyOneClothing.id}>{onlyOneClothing.title}</p>}
     </div>
   )
 }
