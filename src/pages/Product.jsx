af8990b5737af87e8286c8d6f752a157e90dc158
João Oliveira <jaoovit@gmail.com>
2024-07-14 17:49:16 +0100

    create the page to show products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { FaStar } from "react-icons/fa";
 
 const onlyOneClothingURL = "https://fakestoreapi.com/products/"
 
@@ -12,6 +13,7 @@ const Product = () => {
     const res = await fetch(url);
     const data = await res.json();
     setOnlyOneClothing(data)
+    console.log(data)
   }
 
   useEffect(() => {
@@ -19,8 +21,27 @@ const Product = () => {
   }, [])
 
   return (
-    <div>
-      {onlyOneClothing && <p key={onlyOneClothing.id}>{onlyOneClothing.title}</p>}
+    <div className="container flex">
+      {onlyOneClothing && 
+      <div className="flex flex-col items-center justify-between gap-9 lg:flex-row lg:gap-0" key={onlyOneClothing.id}>
+        <div className="px-48">
+         <img src={onlyOneClothing.image} alt={onlyOneClothing.title} />
+        </div>
+        <div className="px-12 flex flex-col gap-9 text-xl">
+          <p>{onlyOneClothing.title}</p>
+          <p>{onlyOneClothing.description}</p>
+          <p>€ {onlyOneClothing.price}</p>
+          <div className="flex items-center gap-2">
+            <FaStar className="text-amber-400"/>
+            <p>{onlyOneClothing.rating.rate}</p>
+          </div>
+          <div className="flex items-center gap-3">
+            <button className="h-9 w-9 bg-black text-white rounded font-bold">+</button>
+            <p>add to card</p>
+          </div>
+        </div>
+      </div>
+      }
     </div>
   )
 }
